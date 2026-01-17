0df8b822548f3c0ce2ee64f745d52c4cb91cc67f
Nicolas Docolas <nicolas.docolas1804@gmail.com>
2026-01-17 16:42:36 -0300

    update ruff checks

--- src/delpro_backend/routes/v1/chat_router.py
+++ src/delpro_backend/routes/v1/chat_router.py
@@ -1,7 +1,10 @@
+"""This module defines the roues for the Chat endpoints."""
 from fastapi import APIRouter
 
 chat_router = APIRouter(prefix="/chat", tags=["chat"])
 
+
 @chat_router.get("/home")
 async def is_alive():
+    """Check if service is alive."""
     return {"detail": "Service is alive!"}
