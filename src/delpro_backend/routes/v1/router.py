bb25bbef396bc735dc12e83eae91162fa3097a8a
Nicolas Docolas <nicolas.docolas1804@gmail.com>
2026-02-21 16:11:31 -0300

    change file name to remove conflict with unittests

--- src/delpro_backend/routes/v1/router.py
+++ src/delpro_backend/routes/v1/router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 
 from delpro_backend.routes.v1.documents_router import documents_router
 from delpro_backend.routes.v1.images_router import images_router
-from delpro_backend.routes.v1.test_router import test_router
+from delpro_backend.routes.v1.evaluate_router import test_router
 from delpro_backend.routes.v1.whatsapp_router import whatsapp_router
 
 router = APIRouter()
